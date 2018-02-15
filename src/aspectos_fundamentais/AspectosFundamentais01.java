f2d8ae38fcac0095a7c6f330d973dd9a7e41d36d
2016122760210 <2016122760210@col-7088-li01.cefetes.br>
2018-02-15 20:12:25 -0200

    Primeiro commit adicionando variaveis

---
+++ src/aspectos_fundamentais/AspectosFundamentais01.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aspectos_fundamentais;
+
+/**
+ *
+ * @author 2016122760210
+ */
+public class AspectosFundamentais01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 1, y = 2;
+        double z = 2.99;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+    
+}
